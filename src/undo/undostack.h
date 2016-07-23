2e07c4d239158d9e699342361fb10e403cde22fa
Mitch Curtis <mitch.curtis@qt.io>
2016-07-23 12:01:05 +0200

    Add properties to UndoStack

--- src/undo/undostack.h
+++ src/undo/undostack.h
@@ -13,6 +13,11 @@ class UndoStackPrivate;
 class Q_UNDO_EXPORT UndoStack : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(bool canUndo READ canUndo NOTIFY canUndoChanged)
+    Q_PROPERTY(bool canRedo READ canRedo NOTIFY canRedoChanged)
+    Q_PROPERTY(QString undoText READ undoText NOTIFY undoTextChanged)
+    Q_PROPERTY(QString redoText READ redoText NOTIFY redoTextChanged)
+    Q_PROPERTY(bool clean READ isClean NOTIFY cleanChanged)
 
 public:
     explicit UndoStack(QObject *parent = nullptr);
